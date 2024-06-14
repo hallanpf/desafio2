826b05634831afa2cfc393f4e4f2792381417b86
hallanpf <hallanpf@gmail.com>
2024-06-14 14:37:29 -0300

    fix: removendo arquivos desnecessários

--- modules/applyFilter.js
+++
@@ -1,6 +0,0 @@
-function applyFilter() {
-  const sortOption = document.getElementById("sort").value;
-  // const perPage = document.getElementById("perPage").value;
-  window.location.href = `/results?sort=${sortOption}`;
-}
-//&perPage=${perPage}
